424e2f643f6e2e31ac1cf04ae439bc23e5d42d73
an-lee <an.lee.work@gmail.com>
2018-12-27 22:10:02 +0800

    redirect to posts

--- app/controllers/circles/home_controller.rb
+++ app/controllers/circles/home_controller.rb
@@ -3,5 +3,6 @@ class Circles::HomeController < Circles::BaseController
   skip_before_action :authenticate_member!
 
   def index
+    redirect_to circle_posts_path(current_circle) if current_circle_user&.member?
   end
 end
