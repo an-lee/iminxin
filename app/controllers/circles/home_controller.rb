cd246fbcfff4eb17991d058b1f61d20f7c0d2a2d
an-lee <an.lee.work@gmail.com>
2019-02-21 09:16:52 +0800

    add antd-mobile

--- app/controllers/circles/home_controller.rb
+++ app/controllers/circles/home_controller.rb
@@ -2,8 +2,6 @@ class Circles::HomeController < Circles::BaseController
   skip_before_action :authenticate_user!
   skip_before_action :authenticate_member!
 
-  layout 'circle_app_home'
-
   def index
     redirect_to circle_posts_path(current_circle) if current_circle_user&.member?
   end
